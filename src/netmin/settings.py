00bdeedea87950c9791afe602ad248ade844a4f9
Matt Scott <matt@azorian.solutions>
2022-12-24 13:40:52 -0500

    Corrected styling convention with settings app index view.

--- src/netmin/settings.py
+++ src/netmin/settings.py
@@ -143,3 +143,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Authentication
+LOGIN_URL = '/account/login'
