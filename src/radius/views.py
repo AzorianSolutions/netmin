68400617c3b4683a46041cdd8ed602150764b82a
Matt Scott <matt@azorian.solutions>
2022-12-28 16:13:50 -0500

    Working on freeRADIUS integration design.

--- src/radius/views.py
+++ src/radius/views.py
@@ -18,8 +18,8 @@ def action_handler(request: HttpRequest, action: str = None):
         reply: list = data['reply']
         reply.append(['Framed-IP-Address', '192.168.70.69'])
         reply.append(['Framed-IP-Netmask', '255.255.255.0'])
-        # reply.append(['Framed-Route', '10.10.11.1/32'])
-        reply.append(['Session-Timeout', '60'])
+        # reply.append(['Framed-Route', '10.10.12.0/29'])
+        reply.append(['Session-Timeout', '300'])
         reply.append(['Acct-Interim-Interval', '15'])
         reply.append(['Mikrotik-Rate-Limit', '2M/20M'])
 
