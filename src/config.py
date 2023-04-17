a9ebd06cb6aadf421a49b68975c40f10c031f846
Matt Scott <matt@azorian.solutions>
2023-04-17 18:20:49 -0400

    Removed orphaned settings from config.py file.
    Added systemd multi-user service definition for Netmin app.

--- src/config.py
+++ src/config.py
@@ -4,8 +4,6 @@ from pydantic import BaseSettings
 class AppSettings(BaseSettings):
     dev_server_address: str = '0.0.0.0'
     dev_server_port: int = 8080
-    graylog_url: str | None
-    graylog_token: str | None
 
     class Config:
-        env_prefix = 'mn_'
+        env_prefix = 'nm_'
